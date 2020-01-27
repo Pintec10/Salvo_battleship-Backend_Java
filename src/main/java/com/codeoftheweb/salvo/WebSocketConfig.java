01dbfca5c68d2aec8392c1a328d2d02a30b623cc
ROBERTO MILANI <robimilani@gmail.com>
2020-01-27 13:25:55 +0100

    200727 cleaned Application, added notes to WebSocket classes

--- src/main/java/com/codeoftheweb/salvo/WebSocketConfig.java
+++ src/main/java/com/codeoftheweb/salvo/WebSocketConfig.java
@@ -13,12 +13,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
+            //enable a simple memory-based message broker to carry the WebSocketMsg
+            //back to the client on destinations prefixed with "/topic"
         config.setApplicationDestinationPrefixes("/comms");
+            //sets the "/comms" prefix for all methods annotated with @MessageMapping
+            //here, client will need to send to "/comms/{gameID}"
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/salvo-websocket")
+                    //endpoint to which the client needs to connect for handshake
                 .setAllowedOrigins("http://localhost:8081",
                         "chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam", //to use APIC extension for Chrome
                         "https://salvogame.netlify.com")
--- src/main/java/com/codeoftheweb/salvo/WebSocketMsgController.java
+++ src/main/java/com/codeoftheweb/salvo/WebSocketMsgController.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketMsgController {
 
-    @MessageMapping("/{gameID}")
-    @SendTo("/topic/{gameID}")
+    @MessageMapping("/{gameID}") // when the client sends messages here, the method below is executed
+    @SendTo("/topic/{gameID}")  // server will send the returned object to this address, which the client subscribed to
     public WebSocketMsg sendUpdateAvailableMsg(@DestinationVariable long gameID) throws Exception {
         System.out.println("Message received. Game ID:");
         System.out.println(gameID);
